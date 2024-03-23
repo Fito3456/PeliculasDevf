f67d452c8902cb14ed959b50f512b1b4c4b01306
Renato <renatolopezflores3456@gmail.com>
2024-03-23 01:34:02 -0300

    se cambio de posicion el archivo

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { RoutesApp } from './src/Routes/RoutesApp'
-import '../Style.css'
-import '../vercel.json'
 
 
 
