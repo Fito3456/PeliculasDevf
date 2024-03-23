5204a78fda0b0ec177f6bf668f3e7c69e5ced524
Renato <renatolopezflores3456@gmail.com>
2024-03-23 02:11:42 -0300

    se agrego el footer

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { NavBar } from '../../Components/Navbar/NavBar'
 import { Outlet } from 'react-router-dom'
-import { Footer } from '../../Components/Footer/Footer'
+
 
 export const Home = () => {
   return (
     <div >
         <NavBar/>
         <Outlet />
-        <Footer/>
+        <h1>Footer</h1>
     </div>
   )
 }
