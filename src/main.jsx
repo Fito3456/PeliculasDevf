aafcdeca9d10fc18c26280dbeabd114bce72e36f
Renato <renatolopezflores3456@gmail.com>
2024-03-23 01:26:45 -0300

    se cambio de posicion el archivo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { RoutesApp } from './src/Routes/RoutesApp'
-import '../Style/Style.css'
+import '../Style.css'
 
 
 
